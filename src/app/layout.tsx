1a9347a45493240605d217ce40950c9842f6739e
Jae-Young98 <ygy0102@gmail.com>
2025-12-14 04:44:55 +0900

    chore: eslint 추가

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,21 +2,15 @@ import "./globals.css";
 import MobileLayout from "@/components/layout/MobileLayout";
 
 export const metadata = {
-  title: "Happy Birthday " + process.env.TITLE_NAME + "🎂",
+  title: "Happy Birthday " + process.env.BIRTHDAY_NAME_EN + "🎂",
   description: "For you",
 };
 
-export default function RootLayout({
-  children,
-}: {
-  children: React.ReactNode;
-}) {
+export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="ko">
       <body>
-        <MobileLayout>
-          {children}
-        </MobileLayout>
+        <MobileLayout>{children}</MobileLayout>
       </body>
     </html>
   );
--- src/components/layout/MobileLayout.tsx
+++ src/components/layout/MobileLayout.tsx
@@ -1,11 +1,3 @@
-export default function MobileLayout({
-    children,
-}: {
-    children: React.ReactNode;
-}) {
-    return (
-        <div className="mx-auto min-h-screen max-w-[430px] bg-white">
-            {children}
-        </div>
-    );
+export default function MobileLayout({ children }: { children: React.ReactNode }) {
+  return <div className="mx-auto min-h-screen max-w-[430px] bg-white">{children}</div>;
 }
